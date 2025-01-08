3ea6a4b21629664f3253e4b3c851de9aab6b362c
Maxime Franck <geeked7@gmail.Com>
2025-01-08 16:41:45 +0100

    Update nice-bust4.cpp

--- components/bus_t4/nice-bust4.cpp
+++ components/bus_t4/nice-bust4.cpp
@@ -55,7 +55,7 @@ namespace esphome {
 
         void NiceBusT4::setup() {
             
-            CanSerial.begin(BAUD_WORK, SERIAL_8N1, 16, 17 );
+            CanSerial.begin(BAUD_WORK, SERIAL_8N1, 17, 16 );
             delay(500);
 
             // who is online?
